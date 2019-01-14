be55ef82fcb8a8a0f4dd55a5dad7fbd26b3a3186
yusuf8421 <yusuf8421@gmail.com>
2019-01-14 16:13:50 +0300

    deneme2

---
+++ LanguageTranslater/LanguageTranslater/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace LanguageTranslater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtSrc = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnAc = new System.Windows.Forms.Button();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.comboAvailableLangs = new System.Windows.Forms.ComboBox();
+            this.txtDestLang = new System.Windows.Forms.TextBox();
+            this.lblSrcLang = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.CmbSpeakers = new Telerik.WinControls.UI.RadDropDownList();
+            ((System.ComponentModel.ISupportInitialize)(this.CmbSpeakers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSrc
+            // 
+            this.txtSrc.Location = new System.Drawing.Point(12, 12);
+            this.txtSrc.Multiline = true;
+            this.txtSrc.Name = "txtSrc";
+            this.txtSrc.Size = new System.Drawing.Size(260, 105);
+            this.txtSrc.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dili Algıla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnAc
+            // 
+            this.BtnAc.Location = new System.Drawing.Point(13, 154);
+            this.BtnAc.Name = "BtnAc";
+            this.BtnAc.Size = new System.Drawing.Size(125, 23);
+            this.BtnAc.TabIndex = 2;
+            this.BtnAc.Text = "Dilleri Getir";
+            this.BtnAc.UseVisualStyleBackColor = true;
+            this.BtnAc.Click += new System.EventHandler(this.BtnAc_Click);
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(145, 153);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(127, 23);
+            this.btnTranslate.TabIndex = 3;
+            this.btnTranslate.Text = "Çevir";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // comboAvailableLangs
+            // 
+            this.comboAvailableLangs.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboAvailableLangs.FormattingEnabled = true;
+            this.comboAvailableLangs.Location = new System.Drawing.Point(12, 184);
+            this.comboAvailableLangs.Name = "comboAvailableLangs";
+            this.comboAvailableLangs.Size = new System.Drawing.Size(260, 21);
+            this.comboAvailableLangs.TabIndex = 4;
+            // 
+            // txtDestLang
+            // 
+            this.txtDestLang.Location = new System.Drawing.Point(13, 212);
+            this.txtDestLang.Multiline = true;
+            this.txtDestLang.Name = "txtDestLang";
+            this.txtDestLang.Size = new System.Drawing.Size(259, 85);
+            this.txtDestLang.TabIndex = 5;
+            // 
+            // lblSrcLang
+            // 
+            this.lblSrcLang.AutoSize = true;
+            this.lblSrcLang.Location = new System.Drawing.Point(12, 304);
+            this.lblSrcLang.Name = "lblSrcLang";
+            this.lblSrcLang.Size = new System.Drawing.Size(0, 13);
+            this.lblSrcLang.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CmbSpeakers
+            // 
+            this.CmbSpeakers.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.CmbSpeakers.Location = new System.Drawing.Point(39, 304);
+            this.CmbSpeakers.Name = "CmbSpeakers";
+            this.CmbSpeakers.Size = new System.Drawing.Size(228, 20);
+            this.CmbSpeakers.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 471);
+            this.Controls.Add(this.CmbSpeakers);
+            this.Controls.Add(this.lblSrcLang);
+            this.Controls.Add(this.txtDestLang);
+            this.Controls.Add(this.comboAvailableLangs);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.BtnAc);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSrc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CmbSpeakers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSrc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnAc;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.ComboBox comboAvailableLangs;
+        private System.Windows.Forms.TextBox txtDestLang;
+        private System.Windows.Forms.Label lblSrcLang;
+        private System.Windows.Forms.Timer timer1;
+        private Telerik.WinControls.UI.RadDropDownList CmbSpeakers;
+    }
+}
+
---
+++ LanguageTranslater/LanguageTranslater/Form1.cs
@@ -0,0 +1,173 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp.Portable.HttpClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net.Http;
+using RestSharp.Portable;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Extensions;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NAudio;
+using NAudio.Wave;
+using System.Linq;
+
+namespace LanguageTranslater
+{
+
+
+    public partial class Form1 : Form
+    {
+        class AppCache
+        {
+
+            public static string API { get; } = @"trnsl.1.1.20190108T085921Z.132ddff657f9e612.9f4c5cbb77785b4f80b7590becef006d9c413593";
+            public static string UrlGetAvailableLanguages { get; } = @"https://translate.yandex.net/api/v1.5/tr.json/getLangs?key={0}&ui={1}";
+            public static string UrlDetectSrcLanguage { get; } = @"https://translate.yandex.net/api/v1.5/tr.json/detect?key={0}&text={1}";
+            public static string UrlTranslateLanguage { get; } = @"https://translate.yandex.net/api/v1.5/tr.json/translate?key={0}&text={1}&lang={2}";
+        }
+
+
+        private List<string> AvailLangs;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var response = RequestService(string.Format(AppCache.UrlDetectSrcLanguage, AppCache.API, txtSrc.Text));
+            var dict = JsonConvert.DeserializeObject<IDictionary>(response.Content);
+
+            var statusCode = dict["code"].ToString();
+            if (statusCode.Equals("200"))
+            {
+                lblSrcLang.Text = dict["lang"].ToString();
+            }
+
+
+        }
+
+
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            string key = "cf807d0e-9aae-445c-88df-570c995c3e91";
+            var response = RequestService(string.Format(AppCache.UrlTranslateLanguage, AppCache.API, txtSrc.Text, AvailLangs[comboAvailableLangs.SelectedIndex]));
+            var dict = JsonConvert.DeserializeObject<IDictionary>(response.Content);
+            var statusCode = dict["code"].ToString();
+            SpeechRecognitionEngine TnmMtr = new SpeechRecognitionEngine();
+            SpeechSynthesizer Sentez = new SpeechSynthesizer();
+            Sentez.SelectVoiceByHints(VoiceGender.Female);
+            if (statusCode.Equals("200"))
+            {
+                txtDestLang.Text = string.Join(separator: ",", value: new string[] { dict["text"].ToString() });
+                //Sentez.SpeakAsync(txtDestLang.Text).ToString();
+                PlayMp3FromUrl("https://tts.voicetech.yandex.net/generate?text=" + txtDestLang.Text + "&speed=1&format=mp3&lang=tr-TR&speaker=" + CmbSpeakers.Text.ToString().Replace(" ", "") + "&emotion=good&key=" + key);
+
+            }
+
+        }
+        public static void PlayMp3FromUrl(string url)
+        {
+
+            using (Stream ms = new MemoryStream())
+            {
+                using (Stream stream = WebRequest.Create(url)
+                    .GetResponse().GetResponseStream())
+                {
+                    byte[] buffer = new byte[32768];
+                    int read;
+                    while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        ms.Write(buffer, 0, read);
+                    }
+                }
+
+                ms.Position = 0;
+                using (WaveStream blockAlignedStream =
+                    new BlockAlignReductionStream(
+                        WaveFormatConversionStream.CreatePcmStream(
+                            new Mp3FileReader(ms))))
+                {
+                    using (WaveOut waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+                    {
+                        waveOut.Init(blockAlignedStream);
+                        waveOut.Play();
+                        while (waveOut.PlaybackState == PlaybackState.Playing)
+                        {
+                            System.Threading.Thread.Sleep(100);
+                        }
+                    }
+                }
+            }
+        }
+        private void BtnAc_Click(object sender, EventArgs e)
+        {
+            var response = RequestService(string.Format(AppCache.UrlGetAvailableLanguages, AppCache.API, lblSrcLang.Text));
+            var dict = JsonConvert.DeserializeObject<IDictionary>(response.Content);
+            foreach (DictionaryEntry entry in dict)
+            {
+                if (entry.Key.Equals("langs"))
+                {
+                    var availableConverts = (JObject) entry.Value;
+                    AvailLangs = new List<string>();
+
+                    comboAvailableLangs.Items.Clear();
+                    foreach (var Lang in availableConverts)
+                    {
+                        if (!Lang.Equals(lblSrcLang.Text))
+                        {
+                            comboAvailableLangs.Items.Add(Lang.Value);
+                            AvailLangs.Add(Lang.Key);
+                        }
+                    }
+                }
+                BtnAc.Enabled = false;
+            }
+
+        }
+        private RestSharp.IRestResponse RequestService(string strUrl)
+        {
+            var client = new RestSharp.RestClient()
+            {
+                BaseUrl = new Uri(strUrl).ToString()
+            };
+            var request = new RestSharp.RestRequest()
+            {
+                Method = RestSharp.Method.GET
+            };
+            return client.Execute(request);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CmbSpeakers.AutoCompleteMode = AutoCompleteMode.Suggest;
+
+            string myString = "jane | oksana | alyss | omazh | zahar | ermil";
+            List<string> myList = myString.Split('|').ToList();
+            
+            foreach (string str in myList)
+            {
+                CmbSpeakers.Items.Add(str);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
