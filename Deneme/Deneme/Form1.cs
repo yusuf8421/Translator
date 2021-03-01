f0ca163e4fd893c10730dbbbe49f66299b232fd7
yusuf8421 <yusuf8421@gmail.com>
2021-03-01 11:50:34 +0300

    mesaj box eklendi

--- Deneme/Deneme/Form1.cs
+++ Deneme/Deneme/Form1.cs
@@ -16,5 +16,10 @@ namespace Deneme
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("denrme");
+        }
     }
 }
